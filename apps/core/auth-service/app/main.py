caed5ff4cba67aceee94930f67d09559487a591e
Ajay Kumar Singh <ajaykumar.kumar900@gmail.com>
2025-12-26 20:22:21 +0530

    feat(db): add postgres to nucles dev cluster

--- apps/core/auth-service/app/main.py
+++ apps/core/auth-service/app/main.py
@@ -1,6 +1,23 @@
+import os
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI(title="Nucleus Auth Service")
+
+def test_db():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+
+    conn.close()
+
+@app.on_event("startup")
+def startup():
+    test_db()
 
 @app.get("/health")
 def health():
